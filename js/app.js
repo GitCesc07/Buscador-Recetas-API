0ee29514caedd8a9009aa18ead065a32cccd76cc
CescDevGit07 <cesar04071997@gmail.com>
2023-04-01 22:24:05 -0600

    Mostrando la información que obtenemos de la API (Receta, Descripción, Imagen)

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 function iniciarApp() {
 
   const selectCategoria = document.querySelector("#categorias");
-  selectCategoria.addEventListener("change", seleccionarCategoria)
+  selectCategoria.addEventListener("change", seleccionarCategoria);
+
+  const resultado = document.querySelector("#resultado")
 
   obtenerCategoria();
 
@@ -37,16 +39,39 @@ function iniciarApp() {
   function mostrarReceta(recetas = []) {
     // Iterar en los resultados
     recetas.forEach(receta => {
-      const { } = receta;
+      const { idMeal, strMeal, strMealThumb } = receta;
 
-      const recetaContenedor = document.createElement("DIV");
+      const recetaContenedor = document.createElement('DIV');
       recetaContenedor.classList.add('col-md-4');
 
-      const recetaCard = document.createElement("DIV");
+      const recetaCard = document.createElement('DIV');
       recetaCard.classList.add('card', 'mb-4');
 
       const recetaImagen = document.createElement('IMG');
       recetaImagen.classList.add('card-img-top');
+      recetaImagen.alt = `Imagen de la receta ${strMeal}`;
+      recetaImagen.src = strMealThumb;
+
+      const recetaCardBody = document.createElement('DIV');
+      recetaCardBody.classList.add('card-body');
+
+      const recetaHeading = document.createElement('H3');
+      recetaHeading.classList.add('card-title', 'mb-3');
+      recetaHeading.textContent = strMeal;
+
+      const recetaButton = document.createElement('BUTTON');
+      recetaButton.classList.add('btn', 'btn-danger', 'w-100');
+      recetaButton.textContent = "Ver receta";
+
+      // Inyectar código de HTML5
+      recetaCardBody.appendChild(recetaHeading)
+      recetaCardBody.appendChild(recetaButton);
+
+      recetaCard.appendChild(recetaImagen);
+      recetaCard.appendChild(recetaCardBody);
+
+      recetaContenedor.appendChild(recetaCard);
+      resultado.appendChild(recetaContenedor);
     })
   }
 }
